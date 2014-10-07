3ae975f75ebc77e90bd71dc7b78effd5251e55e2
Roland Dudko <roland.dudko@gmail.com>
2014-10-07 13:59:29 +0200

    Add command line switch (-v)
    to process only the validation of the configuration file without starting the actual logging process

--- src/brickv/data_logger/main.py
+++ src/brickv/data_logger/main.py
@@ -7,6 +7,9 @@ from brickv.data_logger.data_logger import DataLogger
 import argparse                             # command line argument parser
 import sys
 
+# HashMap keywords to store results of the command line arguments 
+CONSOLE_CONFIG_FILE = "config_file"
+CONSOLE_VALIDATE_ONLY ="validate"
 
 def __exit_condition(data_logger):
     '''
@@ -20,7 +23,7 @@ def __exit_condition(data_logger):
             break
     data_logger.stop(0)
           
-def main(ini_file_path):
+def main(arguments_map):
     '''
     '''
     #initiate the EventLogger
@@ -33,7 +36,9 @@ def main(ini_file_path):
     
     configuration = None
     try:
-        configuration = ConfigurationReader(ini_file_path)
+        configuration = ConfigurationReader(arguments_map[CONSOLE_CONFIG_FILE])
+        if arguments_map[CONSOLE_VALIDATE_ONLY]:
+            return
     except IOError as io_err:
         EventLogger.critical("The parsing of the configuration file failed :" + str(io_err) )
         sys.exit(DataLoggerException.DL_CRITICAL_ERROR)
@@ -47,16 +52,22 @@ def command_line_start(argv,program_name):
     '''
     This function processes the command line arguments, if it was started via the console.
     '''
-    cl_parser = argparse.ArgumentParser(description=' -c <config-file>')
+    cl_parser = argparse.ArgumentParser(description=' -c <config-file> -v ')
     
     cl_parser.add_argument('-c', action="store", dest="config_file", default="None", help="Path to the configuration file")
+    cl_parser.add_argument('-v', action="store_true", dest="validate", help="Just process the validation of the configuration file")
+    
     results = cl_parser.parse_args(argv)
-
-    return results.config_file
+    
+    arguments_map = {}
+    arguments_map[CONSOLE_CONFIG_FILE] = results.config_file
+    arguments_map[CONSOLE_VALIDATE_ONLY] = results.validate
+    
+    return arguments_map
 
 
 from tinkerforge.ip_connection import IPConnection
 ###main###
 if __name__ == '__main__':      
-    ini_file_path = command_line_start(sys.argv[1:], sys.argv[0]) 
-    main(ini_file_path)
+    arguments_map = command_line_start(sys.argv[1:], sys.argv[0]) 
+    main(arguments_map)
