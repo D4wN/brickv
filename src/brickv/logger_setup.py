4189990f2cc19ac3d3743c7fd5b69b0e6080d844
Roland Dudko <roland.dudko@gmail.com>
2015-02-11 14:38:50 +0100

    Add event/signal handling for the data logger config

--- src/brickv/logger_setup.py
+++ src/brickv/logger_setup.py
@@ -1,37 +1,56 @@
-# -*- coding: utf-8 -*-  
-"""
-brickv (Brick Viewer) 
-Copyright (C) 2011-2012 Olaf L�ke <olaf@tinkerforge.com>
-Copyright (C) 2012 Bastian Nordmeyer <bastian@tinkerforge.com>
-Copyright (C) 2012, 2014 Matthias Bolte <matthias@tinkerforge.com>
+# -*- coding: utf-8 -*-
 
-advanced.py: GUI for advanced features
+# Form implementation generated from reading ui file 'logger.ui'
+#
+# Created: Tue Feb 10 14:45:20 2015
+#      by: PyQt4 UI code generator 4.11.1
+#
+# WARNING! All changes made in this file will be lost!
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License 
-as published by the Free Software Foundation; either version 2 
-of the License, or (at your option) any later version.
+from brickv.ui_logger import Ui_Logger
+from PyQt4.QtGui import QDialog
+from PyQt4 import QtGui
+from PyQt4.QtGui import QMessageBox
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public
-License along with this program; if not, write to the
-Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.
-"""
-
-from brickv.ui_logger_setup import Ui_widget_logger_setup
-
-from PyQt4.QtCore import Qt, QTimer
-from PyQt4.QtGui import QFrame
-
-from brickv import infos
-
-class LoggerSetupWindow(QFrame, Ui_widget_logger_setup):
+class LoggerWindow(QDialog,Ui_Logger):
     def __init__(self, parent):
-        QFrame.__init__(self, parent, Qt.Popup | Qt.Window | Qt.Tool)
-
+        QDialog.__init__(self, parent)
         self.setupUi(self)
+        self.signal_initialization()
+        
+        self.show()
+        
+    def signal_initialization(self):
+        # Buttons 
+        self.btn_start_logging.clicked.connect(self.btn_start_logging_clicked)
+        self.btn_save_config.clicked.connect(self.btn_save_config_clicked)
+        self.btn_set_logfile.clicked.connect(self.btn_set_logfile_clicked)
+        
+        self.checkbox_xively.stateChanged.connect(self.cb_xively_changed)
+
+    def btn_start_logging_clicked(self):
+        # TODO: Start data logger here
+        from data_logger import main
+        
+        arguments_map = {}
+        arguments_map[main.CONSOLE_CONFIG_FILE] = self.path_to_config
+        arguments_map[main.CONSOLE_VALIDATE_ONLY] = False
+        main.main(arguments_map)
+        
+    def btn_save_config_clicked(self):
+        # TODO: Call Marvs save_config function
+        QMessageBox.information(self, 'Info', 'btn_save_config_clicked was clicked', QMessageBox.Ok)
+
+    def btn_set_logfile_clicked(self):
+        import os
+        fn = QtGui.QFileDialog.getOpenFileName(self, "Open File...", os.getcwd(),
+                "CSV-Files (*.csv);;Text-Files (*.txt);;JSON-Files (*.json)")
+        if fn:
+            self.lineEdit.setText(fn)
+            self.path_to_config = fn
+    
+    def cb_xively_changed(self):
+        if self.checkbox_xively.isChecked():
+            self.groupBox_xively.setEnabled(True)
+        else:
+            self.groupBox_xively.setEnabled(False)
