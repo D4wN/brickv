758ce779d02dee43e05e6890357fa56076eb6328
Marv <marvin.lutz@fh-bielefeld.de>
2015-02-18 13:08:32 +0100

    added new DeviceDialog to build_ui.py

--- src/brickv/build_ui.py
+++ src/brickv/build_ui.py
@@ -7,6 +7,7 @@ os.system("pyuic4 -o ui_mainwindow.py ui/brickv.ui")
 os.system("pyuic4 -o ui_flashing.py ui/flashing.ui")
 os.system("pyuic4 -o ui_advanced.py ui/advanced.ui")
 os.system("pyuic4 -o ui_logger_setup.py ui/logger_setup.ui")
+os.system("pyuic4 -o ui_device_dialog.py ui/device_dialog.ui")
 
 # Servo Brick
 os.system("pyuic4 -o plugin_system/plugins/servo/ui_servo.py plugin_system/plugins/servo/ui/servo.ui")
