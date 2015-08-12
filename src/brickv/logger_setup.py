7a9c3572d0cebcdca8c87146dce8629ccb36fdd2
Roland Dudko <roland.dudko@gmail.com>
2015-08-12 15:42:19 +0200

    Add error handling

--- src/brickv/logger_setup.py
+++ src/brickv/logger_setup.py
@@ -269,28 +269,31 @@ class LoggerWindow(QDialog, Ui_Logger):
         selectedItem = self.tree_devices.selectedItems()
 
         for index in range(0, len(selectedItem)):
-            device_name = selectedItem[index].text(0)
-            device_id = selectedItem[index].text(1)
+            try:
+                device_name = selectedItem[index].text(0)
+                device_id = selectedItem[index].text(1)
 
-            if selectedItem[index].text(0) not in Identifier.DEVICE_DEFINITIONS:
-                # have to find the parent
-                currentItem = selectedItem[0]
+                if selectedItem[index].text(0) not in Identifier.DEVICE_DEFINITIONS:
+                    # have to find the parent
+                    currentItem = selectedItem[0]
 
-                while True:
-                    if currentItem.parent() is None:
-                        if currentItem.text(0) not in Identifier.DEVICE_DEFINITIONS:
-                            EventLogger.error("Cant remove device: " + selectedItem[index].text(0))
-                            device_name = ""
-                            device_id = ""
-                            break
+                    while True:
+                        if currentItem.parent() is None:
+                            if currentItem.text(0) not in Identifier.DEVICE_DEFINITIONS:
+                                EventLogger.error("Cant remove device: " + selectedItem[index].text(0))
+                                device_name = ""
+                                device_id = ""
+                                break
+                            else:
+                                device_name = currentItem.text(0)
+                                device_id = currentItem.text(1)
+                                break
                         else:
-                            device_name = currentItem.text(0)
-                            device_id = currentItem.text(1)
-                            break
-                    else:
-                        currentItem = currentItem.parent()
+                            currentItem = currentItem.parent()
 
-            self.remove_item_from_tree(device_name, device_id)
+                self.remove_item_from_tree(device_name, device_id)
+            except Exception as e:
+                EventLogger.error("Cant remove device " + str(e))
 
         # if self.logger_device_dialog is None:
         #     self.logger_device_dialog = LoggerDeviceDialog(self)
